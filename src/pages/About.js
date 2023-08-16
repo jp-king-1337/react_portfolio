96d83d80de9c7a289b2a0c74ff6e48aa21443239
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-16 16:30:45 -0400

    Ha. I'm dumb. Just needed another div

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,19 +5,22 @@ export default function About() {
                 <h1 className="text-center">Welcome!</h1>
 
                 <div className="about-content">
-                    <p className="about-paragraph">
-                        I'm J.P. King, and I'm currently transitioning from my the world of restaurants to a career in web development. Originally from Lancaster County, Pennsylvania, I studied German and theatre technology at Lycoming College. After graduating in 2019, I spent a few years working in bustling kitchens before deciding that I need more challenge in my life. I've been interested in coding for years, but never truly dug into it. Looking for a change, I dedicated myself to a coding bootcamp.
-                    </p>
-                    <p className="about-paragraph">
-                        I loved it within the first week. The challenge of learning new things every day, the problem solving inherent in coding, seeing a website take shape as I tap away on my keyboard, all of it. My fascination hasn't faded as I've progressed through the course, and I'm eager to explore new horizons and find a place in the limitless world of web development.
-                    </p>
-                    <p className="about-paragraph">
-                        My journey in the tech world is fueled by an unwavering work ethic, a thirst for continuous learning, and an innate desire to excel. With a strong dedication to self-improvement and a penchant for embracing fresh challenges, I'm committed to staying up-to-date with industry trends and pushing my boundaries. I'm thrilled to become an integral part of the dynamic landscape of web development, and I'm ready to contribute my skills and enthusiasm to bring digital visions to life.
-                    </p>
+                    <div className="about-paragraphs">
+                        <p className="about-paragraph">
+                            I'm J.P. King, and I'm currently transitioning from my the world of restaurants to a career in web development. Originally from Lancaster County, Pennsylvania, I studied German and theatre technology at Lycoming College. After graduating in 2019, I spent a few years working in bustling kitchens before deciding that I need more challenge in my life. I've been interested in coding for years, but never truly dug into it. Looking for a change, I dedicated myself to a coding bootcamp.
+                        </p>
+                        <p className="about-paragraph">
+                            I loved it within the first week. The challenge of learning new things every day, the problem solving inherent in coding, seeing a website take shape as I tap away on my keyboard, all of it. My fascination hasn't faded as I've progressed through the course, and I'm eager to explore new horizons and find a place in the limitless world of web development.
+                        </p>
+                        <p className="about-paragraph">
+                            My journey in the tech world is fueled by an unwavering work ethic, a thirst for continuous learning, and an innate desire to excel. With a strong dedication to self-improvement and a penchant for embracing fresh challenges, I'm committed to staying up-to-date with industry trends and pushing my boundaries. I'm thrilled to become an integral part of the dynamic landscape of web development, and I'm ready to contribute my skills and enthusiasm to bring digital visions to life.
+                        </p>
+                    </div>
+                    <div className="headshot-wrapper">
+                        <img id="headshot" src="/assets/images/headshot.JPG" alt="J.P. King standing in front of a waterfall" />
+                    </div>
                 </div>
-                <div className="headshot-wrapper">
-                    <img id="headshot" src="/assets/images/headshot.JPG" alt="J.P. King standing in front of a waterfall" />
-                </div>
+
 
             </div>
         </>
