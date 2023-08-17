30c9f8d6d1c0ff45c7bcab45bc8f758aa10ad1c9
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-16 23:33:25 -0400

    Added 4 projects

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -1,37 +1,37 @@
 export const projectData = [
     {
+        title: "My First Portfolio",
+        imageSource: "/assets/images/screenshots/screenshot_1FirstPortfolio.png",
+        deployedLink: "https://jp-king-1337.github.io/professional_portfolio/",
+        githubLink: "https://github.com/jp-king-1337/professional_portfolio"
+    },
+    {
+        title: "WeatherFlix",
+        imageSource: "/assets/images/screenshots/screenshot_1WeatherFlix.png",
+        deployedLink: "https://thomasszentirmay.github.io/weather-flix-project/",
+        githubLink: ""
+    },
+    {
+        title: "Travel Circle",
+        imageSource: "/assets/images/screenshots/screenshot_1WeatherFlix.png",
+        deployedLink: "https://sheltered-hollows-60202-cd3cd4a2a48a.herokuapp.com/",
+        githubLink: ""
+    },
+    {
         title: "Project ",
-        imageSource: "",
+        imageSource: "/assets/images/screenshots/screenshot_1GoodVibesFeed.png",
         deployedLink: "",
         githubLink: ""
     },
     {
         title: "Project ",
-        imageSource: "",
+        imageSource: "/assets/images/screenshots/",
         deployedLink: "",
         githubLink: ""
     },
     {
         title: "Project ",
-        imageSource: "",
-        deployedLink: "",
-        githubLink: ""
-    },
-    {
-        title: "Project ",
-        imageSource: "",
-        deployedLink: "",
-        githubLink: ""
-    },
-    {
-        title: "Project ",
-        imageSource: "",
-        deployedLink: "",
-        githubLink: ""
-    },
-    {
-        title: "Project ",
-        imageSource: "",
+        imageSource: "/assets/images/screenshots/",
         deployedLink: "",
         githubLink: ""
     }
