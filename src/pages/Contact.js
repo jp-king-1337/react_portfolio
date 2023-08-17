74d0929a88a600e62e3796fec87f509152efc177
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-16 21:43:28 -0400

    Okay referenced previous work with web3forms and got it sorted. I thought it wasn't working even with the successful console log but then I got the emails

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 export default function Contact() {
     const [errorMessage, setErrorMessage] = useState({
@@ -26,55 +26,72 @@ export default function Contact() {
 
     const isSubmitDisabled = Object.values(errorMessage).some(error => error !== "");
 
-    return (
-        <>
-            <div className="app-container">
-                <h2 className="text-center">Contact Me</h2>
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-                <form className="contact-form" action="https://api.web3forms.com/submit" method="POST" id="form">
-                    <input type="hidden" name="access_key" value="95bb745b-ba15-48d1-9079-3141c573b06c" />
-                    <input type="hidden" name="subject" value="New Submission from Web3Forms" />
-                    <input type="hidden" name="redirect" value="https://web3forms.com/success" />
-                    <input type="checkbox" name="botcheck" id="" style="display: none;" />
-                    <div className="form-group">
-                        <label htmlFor="name">Name</label>
-                        <input
-                            type="text"
-                            id="name"
-                            name="name"
-                            placeholder="What is your name?"
-                            onBlur={handleBlur}
-                        />
-                        <div className="error-message">{errorMessage.name}</div>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="email">Email</label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            placeholder="What is your email?"
-                            onBlur={handleBlur}
-                        />
-                        <div className="error-message">{errorMessage.email}</div>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="message">Message</label>
-                        <textarea
-                            id="message"
-                            name="message"
-                            placeholder="How can I help you?"
-                            onBlur={handleBlur}
-                        />
-                        <div className="error-message">{errorMessage.message}</div>
-                    </div>
-                    <button
-                        type="submit"
-                        disabled={isSubmitDisabled}>
-                        Submit
-                    </button>
-                </form>
-            </div>
-        </>
+        const formData = new FormData(e.target);
+        formData.append("access_key", "95bb745b-ba15-48d1-9079-3141c573b06c");
+
+        fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    console.log("Form submitted successfully");
+                } else {
+                    console.error("Form submission failed");
+                }
+            })
+            .catch(error => {
+                console.error("An error occurred while submitting the form:", error);
+            });
+    };
+
+    return (
+        <div className="app-container">
+            <h2 className="text-center">Contact Me</h2>
+
+            <form className="contact-form" onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="What is your name?"
+                        onBlur={handleBlur}
+                    />
+                    <div className="error-message">{errorMessage.name}</div>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="What is your email?"
+                        onBlur={handleBlur}
+                    />
+                    <div className="error-message">{errorMessage.email}</div>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="message">Message</label>
+                    <textarea
+                        id="message"
+                        name="message"
+                        placeholder="How can I help you?"
+                        onBlur={handleBlur}
+                    />
+                    <div className="error-message">{errorMessage.message}</div>
+                </div>
+                <button
+                    type="submit"
+                    disabled={isSubmitDisabled}>
+                    Submit
+                </button>
+            </form>
+        </div>
     );
 }
