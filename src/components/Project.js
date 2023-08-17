e4c886ddd6dcc85cfd63bcfe09da5d92b61cbff5
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-17 00:36:42 -0400

    Updated classes in Project componenent. I like my classes named with dashes, ids camelCase.

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,10 @@
 export default function Project({ title, imageSource, description, deployedLink, githubLink }) {
     return (
-        <div className="project">
-            <h2 className="projectTitle">{title}</h2>
-            <img className="projectImage" src={imageSource} alt={title} />
-            <p className="projectDescription">{description}</p>
-            <div className="projectLinks">
+        <div className="project-card">
+            <h2 className="project-title">{title}</h2>
+            <img className="project-image" src={imageSource} alt={title} />
+            <p className="project-description">{description}</p>
+            <div className="project-links">
                 <a href={deployedLink} target="_blank" rel="noopener noreferrer">Deployed Application</a>
                 <a href={githubLink} target="_blank" rel="noopener noreferrer">GitHub Repository</a>
             </div>
