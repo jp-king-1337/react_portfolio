2fdf5144bae2d4b59c31f68c605430b8b2768f7f
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-16 22:59:17 -0400

    Crap. Gotta look into this more. I need a source for my projects but I'm not sure the best way. Maybe an array, but idk if a different file or Project component or the Portfolio page itself should hold it

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,8 +1,13 @@
+import Project from "../components/Project"
+
 export default function Portfolio() {
     return (
         <>
             <div className="app-container">
                 <h1 className="text-center">This is a portfolio page.</h1>
+                <div className="project-list">
+                    {/* ??????? */}
+                </div>
             </div>
         </>
     )
