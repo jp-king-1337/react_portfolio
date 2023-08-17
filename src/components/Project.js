e8dddc3e4c0f169606e19630dad051f27bde26c5
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-16 23:04:52 -0400

    Added specificity in the Project html classes

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,9 @@
 export default function Project({ title, imageSource, deployedLink, githubLink }) {
     return (
         <div className="project">
-            <h2>{title}</h2>
-            <img src={imageSource} alt={title} />
-            <div className="links">
+            <h2 className="projectTitle">{title}</h2>
+            <img className="projectImage" src={imageSource} alt={title} />
+            <div className="projectLinks">
                 <a href={deployedLink} target="_blank" rel="noopener noreferrer">Live</a>
                 <a href={githubLink} target="_blank" rel="noopener noreferrer">GitHub</a>
             </div>
