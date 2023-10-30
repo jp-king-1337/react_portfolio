ccf419d414cfcdccf4498f9f57c3fd8cf07d3b00
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-10-30 15:11:40 -0400

    Updating deployed and GitHub links. Now they only link to my GitHub. Group projects have been forked so I don't link to anyone else's page - minimizes confusion. Updated project descriptions as well.

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -2,7 +2,7 @@ export const projectData = [
     {
         title: "My First Portfolio",
         imageSource: "/assets/images/screenshots/screenshot_1FirstPortfolio.png",
-        description: "This was my first portfolio website, which was one of the first websites I ever built.",
+        description: "Want to see how far I've come? This was my first portfolio website, which was one of the first websites I ever built.",
         deployedLink: "https://jp-king-1337.github.io/professional_portfolio/",
         githubLink: "https://github.com/jp-king-1337/professional_portfolio"
     },
@@ -10,34 +10,34 @@ export const projectData = [
         title: "Weather Flix",
         imageSource: "/assets/images/screenshots/screenshot_1WeatherFlix.png",
         description: "Not sure what movie to watch? I developed this along with other students in my bootcamp to suggest movies based on the current weather in your area.",
-        deployedLink: "https://thomasszentirmay.github.io/weather-flix-project/",
-        githubLink: "https://github.com/ThomasSzentirmay/weather-flix-project"
+        deployedLink: "https://jp-king-1337.github.io/weather-flix-project/",
+        githubLink: "https://github.com/jp-king-1337/weather-flix-project"
     },
     {
         title: "Travel Circle",
         imageSource: "/assets/images/screenshots/screenshot_1TravelCircle.png",
         description: "Me and a small group of students made this website as a private travel journal where you can share your journeys with your friends and family.",
         deployedLink: "https://sheltered-hollows-60202-cd3cd4a2a48a.herokuapp.com/",
-        githubLink: "https://github.com/MKYRENE/travel_circle_app"
+        githubLink: "https://github.com/jp-king-1337/travel_circle_app"
     },
     {
         title: "Good Vibes Feed",
         imageSource: "/assets/images/screenshots/screenshot_1GoodVibesFeed.png",
-        description: "The Good Vibes Feed was developed by myself and four other students to bring you a small news feed consisting of only good news!",
+        description: "Tired of all the doom and gloom? The Good Vibes Feed was developed by myself and four other students to bring you a small news feed consisting of only good news!",
         deployedLink: "https://good-vibes-feed-0f727cb91a84.herokuapp.com/",
-        githubLink: "https://github.com/mcgrane18482/good_vibes_feed"
+        githubLink: "https://github.com/jp-king-1337/good_vibes_feed"
     },
     {
         title: "Just Blog It!",
         imageSource: "/assets/images/screenshots/screenshot_1JustBlogIt.png",
-        description: "I built this small blog site to hold a variety of tech-related articles. Maybe you can use it to share your coding journey with the world!",
+        description: "Looking to share your tech journey with the world? I built this small blog site to hold a variety of tech-related articles.",
         deployedLink: "https://jp-just-blog-it-ae6e976d19e8.herokuapp.com/",
         githubLink: "https://github.com/jp-king-1337/justBlogIt"
     },
     {
         title: "Text Edit - If You Dare!",
         imageSource: "/assets/images/screenshots/screenshot_1TextEdit.png",
-        description: "This is a refactor of some starter code provided to me in my coding bootcamp. My job was to implement PWA functionality!",
+        description: "Got some text to edit? This is a refactor of some starter code provided to me in my coding bootcamp. My job was to implement PWA functionality!",
         deployedLink: "https://text-edit-if-you-dare-288bba82860c.herokuapp.com/",
         githubLink: "https://github.com/jp-king-1337/textEdit_ifYouDare"
     }
