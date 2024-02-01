181b14aa0cf43a45cb5d40cc486763b55b3fce78
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2024-02-01 15:11:19 -0500

    I think I'm happy with the skills section for now. Adjusted the about section a bit, working on the photo now

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,16 +2,19 @@ export default function About() {
     return (
         <>
             <div className="app-container">
-                <h2 className="text-center">Welcome!</h2>
-
                 <div className="about-content">
-                    <div className="about-paragraphs">
-                        <p className="about-paragraph">
-                            I'm J.P. King, a Web Developer from Pennsylvania with a diverse background in German studies and the restaurant industry. My journey in tech is fueled by an unwavering work ethic and a thirst for continuous learning. I am eager to contribute my skills and enthusiasm to the dynamic world of web development. Let's build something amazing together!
-                        </p>
+                    <div className="left-section">
+                        <div className="headshot-wrapper">
+                            <img id="headshot" src="/assets/images/headshot.JPG" alt="J.P. King standing in front of a waterfall" />
+                        </div>
                     </div>
-                    <div className="headshot-wrapper">
-                        <img id="headshot" src="/assets/images/headshot.JPG" alt="J.P. King standing in front of a waterfall" />
+                    <div className="right-section">
+                        <h2 id="welcome" className="text-center">Welcome!</h2>
+                        <div className="about-paragraphs">
+                            <p className="about-paragraph">
+                                I'm J.P. King, a Web Developer from Pennsylvania with a diverse background in German studies and the restaurant industry. My journey in tech is fueled by an unwavering work ethic and a thirst for continuous learning. I am eager to contribute my skills and enthusiasm to the dynamic world of web development. Let's build something amazing together!
+                            </p>
+                        </div>
                     </div>
                 </div>
 
