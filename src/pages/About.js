ecfa62239ff7e70c5066c5da2ce13bbd1a9fbf09
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2024-02-01 14:30:41 -0500

    Added skill logos with alt text

--- src/pages/About.js
+++ src/pages/About.js
@@ -17,18 +17,19 @@ export default function About() {
 
                 <div className="skills-container">
                     <h2>Skills</h2>
-                    <img src="html5_logo_url" alt="HTML5 Logo"></img>
-                    <img class="skill-logo" src="/assets/images/logos/html5.svg" alt="HTML icon"></img>
-                    <img src="css3_logo_url" alt="CSS3 Logo"></img>
-                    <img src="javascript_es6_logo_url" alt="JavaScript ES6 Logo"></img>
-                    <img src="react_logo_url" alt="React Logo"></img>
-                    <img src="express_logo_url" alt="Express Logo"></img>
-                    <img src="rest_api_logo_url" alt="REST API Logo"></img>
-                    <img src="mongodb_logo_url" alt="MongoDB Logo"></img>
-                    <img src="authentication_logo_url" alt="Authentication Logo"></img>
-                    <img src="github_logo_url" alt="GitHub Logo"></img>
-                    <img src="git_logo_url" alt="Git Logo"></img>
-                    <img src="terminal_logo_url" alt="Terminal Logo"></img>
+                    <img class="skill-logo" src="/assets/images/logos/html5.svg" alt="HTML5 icon"></img>
+                    <img class="skill-logo" src="/assets/images/logos/css3.svg" alt="CSS3 icon"></img>
+                    <img class="skill-logo" src="/assets/images/logos/express.svg" alt="Express icon"></img>
+                    <img class="skill-logo" src="/assets/images/logos/git.svg" alt="Git icon"></img>
+                    <img class="skill-logo" src="/assets/images/logos/github.svg" alt="GitHub icon"></img>
+                    <img class="skill-logo" src="/assets/images/logos/javascript.svg" alt="JavaScript icon"></img>
+                    <img class="skill-logo" src="/assets/images/logos/mongodb-plain.svg" alt="MongoDB icon"></img>
+                    <img class="skill-logo" src="/assets/images/logos/react.svg" alt="React icon"></img>
+                    <img class="skill-logo" src="/assets/images/logos/sequelize.svg" alt="Sequelize icon"></img>
+                    <img class="skill-logo" src="/assets/images/logos/vscode.svg" alt="VSCode icon"></img>
+
+
+
                 </div>
             </div>
         </>
